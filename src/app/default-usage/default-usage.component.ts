513f70a489316b7750421c07ae8f1eb5d3628c5c
Dennis Fokker <dennis.fokker@live.nl>
2018-10-20 10:49:16 +0200

    Added base of default usage.

---
+++ src/app/default-usage/default-usage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-usage',
+  templateUrl: './default-usage.component.html',
+  styleUrls: ['./default-usage.component.css']
+})
+export class DefaultUsageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
