6707e64ab2e2c0681f1a6b8409dcc8d13381c16e
Dennis Fokker <dennis.fokker@live.nl>
2018-10-20 11:40:41 +0200

    Added base of recipes + minor fixes.

--- src/app/default-usage/default-usage.component.ts
+++ src/app/default-usage/default-usage.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-default-usage',
-  templateUrl: './default-usage.component.html',
-  styleUrls: ['./default-usage.component.css']
+    selector: 'app-default-usage',
+    templateUrl: './default-usage.component.html',
+    styleUrls: ['./default-usage.component.css']
 })
-export class DefaultUsageComponent implements OnInit {
+export class DefaultUsageComponent implements OnInit
+{
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit()
+    {
+    }
 
 }
